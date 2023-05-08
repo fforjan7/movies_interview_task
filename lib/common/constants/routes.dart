578bf479f93e1b844cf2b5161a867f5d81ace0c2
fforjan7 <filip.forjan7@gmail.com>
2023-05-08 22:51:40 +0200

    goRouter added

---
+++ lib/common/constants/routes.dart
@@ -0,0 +1,5 @@
+abstract class AppRoutes {
+  static const String splash = '/';
+  static const String home = '/home';
+  static const String details = '/details';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:go_router/go_router.dart';
 import 'package:movies_interview_task/common/constants/constants.dart';
+import 'package:movies_interview_task/ui/pages/splash/splash_page.dart';
+
+import 'common/constants/routes.dart';
 
 Future<void> main() async {
   await dotenv.load();
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     print(dotenv.get(AppConstants.token));
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      title: "Interview task",
+      routerConfig: _router,
+      debugShowCheckedModeBanner: false,
     );
   }
+
+  final GoRouter _router = GoRouter(routes: <GoRoute>[
+    GoRoute(
+      path: AppRoutes.splash,
+      builder: (BuildContext context, GoRouterState state) {
+        return const SplashPage();
+      },
+    ),
+  ]);
 }
